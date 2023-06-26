2e49eff3537e5b6dc24798f373606421a4915664
Gianluca Bellocchi <gbellocchi95@unimore.it>
2023-06-26 12:03:58 +0200

    Checked SW benchmark + Added pre-build

--- benchmarks/host/arm64/common/board/board.mk
+++ benchmarks/host/arm64/common/board/board.mk
@@ -1,4 +1,8 @@
-app_deploy: clean_board
+run_app:
+	@echo -e "Running '$(APP_NAME)' on '$(HERO_TARGET_HOST):$(HERO_TARGET_PATH_APPS)/$(APP_NAME)'"
+	@ssh -t $(HERO_TARGET_HOST) 'cd ${HERO_TARGET_PATH_APPS}; ./$(APP_NAME) $(RUN_ARGS)'
+
+deploy_app: clean_board
 	@echo -e "Deploying '$(APP_NAME)' to '$(HERO_TARGET_HOST):$(HERO_TARGET_PATH_APPS)/$(APP_NAME)'"
 	@scp $(BUILD_DIR)/$(APP_NAME) $(HERO_TARGET_HOST):$(HERO_TARGET_PATH_APPS)
 
